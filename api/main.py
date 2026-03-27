293d08f3af995654cd98ca82f5afa8f4c6627c7d
nishnarudkar <nishnarudkar@gmail.com>
2026-03-28 02:51:56 +0530

    Fix TemplateResponse signature for Starlette 0.40+

--- api/main.py
+++ api/main.py
@@ -151,7 +151,7 @@ class FeatureInput(BaseModel):
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
 
 
 @app.post("/predict")
